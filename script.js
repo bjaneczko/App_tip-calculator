c04867e919ca66254fce349357e2a3f4542d6b91
BJaneczko <77055945+bjaneczko@users.noreply.github.com>
2021-11-04 01:20:40 +0100

    Refactoring code

--- script.js
+++ script.js
@@ -1,41 +1,24 @@
 'use strict';
 
+//// Expresions
+const form = document.querySelector('form');
 const numPeople = document.getElementById('numPeople');
 const bill = document.getElementById('bill');
-const tipAmount = document.getElementById('tipAmount');
-const totalAmount = document.getElementById('totalAmount');
+const tip = document.getElementById('tip');
+const total = document.getElementById('total');
 const resetBtn = document.getElementById('resetBtn');
-const alertText = document.getElementById('alertText');
+const textAlert = document.getElementById('textAlert');
+const buttons = document.querySelectorAll('button');
+const custom = document.getElementById('custom');
 
-const form = document.querySelector('form');
-
-form.addEventListener('submit', e => {
-  e.preventDefault(); // Prevent site from reloading after submiting a form
-  if (numPeople.value > 0) {
-    numPeople.style.setProperty('--some-color', 'hsl(172, 67%, 45%)');
-    numPeople.classList.remove('input--alert');
-    alertText.classList.add('hide');
-    const percent = 10;
-    let tip = (bill.value * (percent / 100)).toFixed(2);
-    console.log(tip);
-    tipAmount.innerHTML = `$${tip / numPeople.value}`;
-    let total = Number(tip) + Number(bill.value);
-    totalAmount.innerHTML = `$${total / numPeople.value}`;
-    console.log(total);
-  } else {
-    numPeople.style.setProperty('--some-color', 'red');
-    numPeople.classList.add('input--alert');
-    alertText.classList.remove('hide');
-  }
-});
+//// Functionality
+resetBtn.addEventListener('click', reset);
 
 function reset() {
   bill.value = 0;
   numPeople.value = 0;
-  tipAmount.innerHTML = '$0.00';
-  totalAmount.innerHTML = '$0.00';
+  tip.innerHTML = '$0.00';
+  total.innerHTML = '$0.00';
   numPeople.classList.remove('input--alert');
-  alertText.classList.add('hide');
+  textAlert.classList.add('hide');
 }
-
-resetBtn.addEventListener('click', reset);
