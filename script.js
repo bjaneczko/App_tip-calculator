38ea8b86eca98ae8b0dfdd0e57a6640d722f09ca
BJaneczko <77055945+bjaneczko@users.noreply.github.com>
2021-11-09 12:19:24 +0100

    JS functionality

--- script.js
+++ script.js
@@ -1,24 +1,68 @@
 'use strict';
 
 //// Expresions
-const form = document.querySelector('form');
-const numPeople = document.getElementById('numPeople');
-const bill = document.getElementById('bill');
-const tip = document.getElementById('tip');
-const total = document.getElementById('total');
-const resetBtn = document.getElementById('resetBtn');
-const textAlert = document.getElementById('textAlert');
-const buttons = document.querySelectorAll('button');
+const bill = document.getElementById('bill'); //// Functionality
+const tipBtns = document.querySelectorAll('btn--tip');
 const custom = document.getElementById('custom');
+const people = document.getElementById('people');
+const alert = document.querySelector('.label--alert');
+const results = document.querySelectorAll('.result__output');
+const resetBtn = document.querySelector('.btn--reset');
 
-//// Functionality
+//default values
+
+let billValue = 0.0;
+let peopleValue = 1;
+let tipValue = 0.15; // 15% default
+
+//// Event listeners
+bill.addEventListener('input', setBillValue);
+tipBtns.forEach(btn => {
+  btn.addEventListener('click', handleClick);
+});
 resetBtn.addEventListener('click', reset);
 
+//// Functions
+
+function setBillValue() {
+  if (bill.value.includes(',')) {
+    bill.value = bill.value.replace(',', '.');
+  }
+
+  billValue = parseFloat(bill.value);
+
+  calculateTip();
+  // console.log(billValue);
+}
+
+function handleClick(event) {
+  tipBtns.forEach(btn => {
+    //clear active state
+    btn.classList.remove('btn--active');
+    console.log('class removed');
+
+    //set active state
+    if (event.target.innerHTML == btn.innerHTML) {
+      btn.classList.add('btn--active');
+      tipValue = parseFloat(btn.innerHTML) / 100;
+    }
+  });
+
+  //clear custom tip
+  custom.value = '';
+
+  calculateTip();
+
+  //console.log(tipValue);
+}
+
+function calculateTip() {}
+
 function reset() {
-  bill.value = 0;
-  numPeople.value = 0;
-  tip.innerHTML = '$0.00';
-  total.innerHTML = '$0.00';
-  numPeople.classList.remove('input--alert');
-  textAlert.classList.add('hide');
+  bill.value = '0';
+  setBillValue();
+
+  tipBtns[2].click();
+
+  people.value = '1';
 }
